e62499f45bbd675d7b4a93c8a93a3c1952f68cbe
gregmelo <gregoryvericel6@gmail.com>
2024-05-24 13:48:47 +0200

    Ajout des commentaires dans le fichier modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,10 +11,12 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//On récupère l'élément de la croix qui permet de fermé la modale
 const modalClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//On écoute le click sur la croix pour fermer la modale
 modalClose.addEventListener("click", closeModal);
 
 // launch modal form
@@ -22,6 +24,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Fonction qui permet de fermer la modale
 function closeModal() {
   modalbg.style.display = "none";
 }
