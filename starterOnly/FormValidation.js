a4e0736f9f3aa82921775af5ed15d42778197164
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2024-07-11 13:19:28 +0200

    CSS Issue #3

--- starterOnly/FormValidation.js
+++ starterOnly/FormValidation.js
@@ -26,17 +26,26 @@ function validate() {
   locationError.textContent = '';
   termsError.textContent = '';
 
+// Réinitialiser les bordures des champs
+  firstName.classList.remove('error');
+  lastName.classList.remove('error');
+  email.classList.remove('error');
+  birthdate.classList.remove('error');
+  quantity.classList.remove('error');
+
   let valid = true;
 
   // Vérifier le champ Prénom (minimum 2 caractères / n'est pas vide)
   if (firstName.value.trim().length < 2) {
     firstError.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
+    firstName.classList.add('error');
     valid = false;
   }
 
   // Vérifier le champ Nom (minimum 2 caractères / n'est pas vide)
   if (lastName.value.trim().length < 2) {
     lastError.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+    firstName.classList.add('error');
     valid = false;
   }
 
@@ -44,18 +53,21 @@ function validate() {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(email.value)) {
     emailError.textContent = 'Veuillez entrer une adresse électronique valide.';
+    firstName.classList.add('error');
     valid = false;
   }
 
   // Vérifier le champ de la date de naissance (n'est pas vide)
   if (birthdate.value.trim() === '') {
     birthdateError.textContent = 'Vous devez entrer votre date de naissance.';
+    firstName.classList.add('error');
     valid = false;
   }
 
   // Vérifier le champ de la quantité (une valeur numérique est saisie)
   if (isNaN(quantity.value) || quantity.value.trim() === '') {
     quantityError.textContent = 'Veuillez entrer une valeur numérique pour le nombre de tournois.';
+    firstName.classList.add('error');
     valid = false;
   }
 
@@ -76,6 +88,31 @@ function validate() {
   if (!terms.checked) {
     termsError.textContent = 'Vous devez vérifier que vous acceptez les termes et conditions.';
     valid = false;
+  }
+
+  // Retirer la classe 'error' des champs valides
+  if (firstName.value.trim().length >= 2) {
+    firstName.classList.remove('error');
+  }
+  if (lastName.value.trim().length >= 2) {
+    lastName.classList.remove('error');
+  }
+  if (emailPattern.test(email.value)) {
+    email.classList.remove('error');
+  }
+  if (birthdate.value.trim() !== '') {
+    birthdate.classList.remove('error');
+  }
+  if (!isNaN(quantity.value) && quantity.value.trim() !== '') {
+    quantity.classList.remove('error');
+  }
+  if (locationSelected) {
+    for (const location of locations) {
+      location.classList.remove('error');
+    }
+  }
+  if (terms.checked) {
+    terms.classList.remove('error');
   }
 
   return valid;
